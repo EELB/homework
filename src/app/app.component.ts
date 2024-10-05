4a430334978c52f4701c209d3c28400a0717d067
AlexisSM377 <al222111377@gmail.com>
2024-10-04 23:38:17 -0600

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Subject } from './model';
+import { SUBJECTS_INFO } from './data';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'homework-app';
+  subjects: Subject[] = SUBJECTS_INFO
+  subjectId = ''
+
+  handleSelectSubjectId = (id: string) => {
+    this.subjectId = id
+  }
+
+  get selectedSubject() {
+    return this.subjects.find(item => item.id === this.subjectId)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SubjectsComponent } from './components/subjects/subjects.component';
+import { CommonHeaderComponent } from './components/shared/common-header/common-header.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { FormTaskComponent } from './components/tasks/form-task/form-task.component';
+import { TaskComponent } from './components/tasks/task/task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SubjectsComponent,
+    CommonHeaderComponent,
+    TasksComponent,
+    FormTaskComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule,
